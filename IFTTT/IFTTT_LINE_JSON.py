b1853616306d4444b1b6d4bdd504ba1a14aa3a58
JackMAxuw <jackie1129008@gmail.com>
2022-07-18 21:28:11 +0800

    change sendLine function to class DataBaseManager

--- IFTTT/IFTTT_LINE_JSON.py
+++ IFTTT/IFTTT_LINE_JSON.py
@@ -2,23 +2,32 @@ import requests
 import json
 
 
-LINE_KEY = "fAf1bFGa2-3np-NztbgltNCQfj3ew3ha6gx7PwBm9IQ"
-LINE_EVENT = "test_json"
-
 CONTEXT = [
-    {"name": "cherry", "price": "10"},
-    {"name": "apple", "price": "50"},
-    {"name": "note", "price": "100"},
-    {"name": "cup", "price": "40"},
+    {"name": "mouse", "price": 499},
+    {"name": "keyboard", "price": 799},
+    {"name": "laptop", "price": 40000},
+    {"name": "cup", "price": 199},
+    {"name": "chair", "price": 500},
 ]
 
 
-def sendLine (key: str, event: str, context: dict) -> None:
+class DataBaseManager():
+    def __init__(self) -> None:
+        self.line_key = "fAf1bFGa2-3np-NztbgltNCQfj3ew3ha6gx7PwBm9IQ"
+        self.line_event = "sendLineJson"
+        self.sheet_key = "fAf1bFGa2-3np-NztbgltNCQfj3ew3ha6gx7PwBm9IQ"
+        self.sheet_event = "sendSheets"
 
-    # Your IFTTT LINE_URL with event name, key and json parameters (values)
-    LINE_URL=f"https://maker.ifttt.com/trigger/{LINE_EVENT}/json/with/key/{LINE_KEY}"
-    # session.post(LINE_URL, data=context_json, headers={'Content-Type': 'application/json'})
-    r = requests.post(LINE_URL, data=json.dumps(context), headers={'Content-Type': 'application/json'})
+    def sendLine(key: str, event: str, context: dict) -> None:
 
+        # Your IFTTT LINE_URL with event name, key and json parameters (values)
+        LINE_URL = f"https://maker.ifttt.com/trigger/{LINE_EVENT}/json/with/key/{LINE_KEY}"
+        # session.post(LINE_URL, data=context_json, headers={'Content-Type': 'application/json'})
+        r = requests.post(LINE_URL, data=json.dumps(context), headers={
+            'Content-Type': 'application/json'})
 
-sendLine(LINE_KEY, LINE_EVENT, CONTEXT)
+    def getSheet() -> None:
+        pass
+
+    def sendSheet(key: str, event: str, context: dict) -> None:
+        pass
